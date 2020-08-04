91a70ea99ec6a5b9d9b5cea388cefc26590a7db6
Mohsin <meghani.mohsin@gmail.com>
2020-08-04 22:23:55 +0500

    First Commit

---
+++ Online Examination System/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Examination_System.Startup))]
+namespace Online_Examination_System
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
