4964d6d45d4c771f5e62837ddeb63b56c9c39f9d
Mohsin <meghani.mohsin@gmail.com>
2020-08-18 16:25:59 +0500

    User Class Added OES_DAL

---
+++ OES_BAL/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OES_BAL
+{
+    class Course
+    {
+
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public Program Program { get; set; }
+
+
+    }
+}
---
+++ OES_BAL/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OES_BAL
+{
+    class Program
+    {
+        public int ID { get; set; }
+        public String Code { get; set; }
+        public string Name { get; set; }
+        public String Details { get; set; }
+
+        public Program()
+        {
+        }
+
+        public void ADD()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+
+        public void Update()
+        {
+
+        }
+
+    }
+}
